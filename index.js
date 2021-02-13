d18bbca5e9e8d0eae585c101d4a8af13a863b538
dgymjol <65115422+dgymjol@users.noreply.github.com>
2021-02-13 21:40:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 const config = require('./config/key');
 
 const mongoose = require('mongoose')
-mongoose.connect('config.mongoURI', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology : true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log('MongoDB Connected...'))
   .catch(err=> console.log(err))
